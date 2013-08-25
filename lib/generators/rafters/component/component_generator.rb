44ff647731c3788236e689dca71d3a4893a7d42d
Andrew Hite <ahite@getsatisfaction.com>
2013-08-25 01:56:37 -0500

    Add install and component generators

---
+++ lib/generators/rafters/component/component_generator.rb
@@ -0,0 +1,33 @@
+class Rafters::ComponentGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path("../templates", __FILE__)
+
+  def create_directories
+    empty_directory "#{base_directory}"
+    empty_directory "#{base_directory}/assets"
+    empty_directory "#{base_directory}/assets/javascripts"
+    empty_directory "#{base_directory}/assets/stylesheets"
+    empty_directory "#{base_directory}/assets/images"
+    empty_directory "#{base_directory}/views"
+  end
+
+  def create_gitkeeps
+    create_file "#{base_directory}/assets/images/.gitkeep"
+  end
+
+  def create_files
+    template "component.rb.erb", "#{base_directory}/#{file_name}_component.rb"
+    template "assets/javascripts/component.js.erb", "#{base_directory}/assets/javascripts/#{file_name}_component.js"
+    template "assets/stylesheets/component.scss.erb", "#{base_directory}/assets/stylesheets/#{file_name}_component.scss"
+    template "views/component.html.erb", "#{base_directory}/views/#{file_name}_component.html.erb"
+  end
+
+  private
+
+  def app_root
+    Rails.root
+  end
+
+  def base_directory
+    "app/components/#{file_name}"
+  end
+end
---
+++ lib/generators/rafters/install/install_generator.rb
@@ -0,0 +1,11 @@
+class Rafters::InstallGenerator < Rails::Generators::Base
+  source_root File.expand_path("../templates", __FILE__)
+
+  def create_initializer
+    copy_file "initializer.rb", "config/initializers/rafters.rb"
+  end
+
+  def create_directories
+    empty_directory "app/components"
+  end
+end
