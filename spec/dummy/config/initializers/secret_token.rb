188fe186f4eecf0ce7e222e8419f9121d029c047
Andrew Hite <ahite@getsatisfaction.com>
2013-08-25 17:37:32 -0500

    Update things for Rails 4

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '304950c1cbff2fec75b62ede6d392171fc25435481002bd7ba9d911433a277bb96aed01bf90572d18f819d20a5ec45a835ccfbfe42347f0c3a0fb527e543c694'
+Dummy::Application.config.secret_key_base = '12345'
