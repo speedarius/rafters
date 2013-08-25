cf1a3925966105d303bb175d56178299c1e7988d
Andrew Hite <ahite@getsatisfaction.com>
2013-08-25 01:56:14 -0500

    Change component directory name to components (previously rafters)

--- lib/rafters/engine.rb
+++ lib/rafters/engine.rb
@@ -8,10 +8,10 @@ class Rafters::Engine < Rails::Engine
   end
 
   initializer "rafters.load_view_paths", :after => "rafters.load_app_root" do |app|
-    Rafters.view_paths = Dir[app.root.join("app", "rafters", "*", "views")]
+    Rafters.view_paths = Dir[app.root.join("app", "components", "*", "views")]
   end
 
   initializer "rafters.set_autoload_paths", :before => :set_autoload_paths do |app|
-    app.config.autoload_paths += Dir[app.root.join("app", "rafters", "*/")]
+    app.config.autoload_paths += Dir[app.root.join("app", "components", "*/")]
   end
 end
