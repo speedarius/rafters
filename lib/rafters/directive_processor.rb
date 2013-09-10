429ee51f9f4e99a59f5abeea21fbab74fd14d808
Andrew Hite <ahite@getsatisfaction.com>
2013-09-10 15:59:26 -0500

    Explicitly declare Rafters namespace in directive processor

--- lib/rafters/directive_processor.rb
+++ lib/rafters/directive_processor.rb
@@ -1,10 +1,12 @@
 require 'sprockets'
 
-class Rafters::DirectiveProcessor < Sprockets::DirectiveProcessor
-  def process_require_components_directive
-    Rafters.asset_paths.sort.each do |asset_path|
-      each_entry(asset_path) do |pathname|
-        context.require_asset(pathname) if context.asset_requirable?(pathname)
+module Rafters
+  class DirectiveProcessor < Sprockets::DirectiveProcessor
+    def process_require_components_directive
+      Rafters.asset_paths.sort.each do |asset_path|
+        each_entry(asset_path) do |pathname|
+          context.require_asset(pathname) if context.asset_requirable?(pathname)
+        end
       end
     end
   end
