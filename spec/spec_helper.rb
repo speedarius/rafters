4b9906bf98efc80081b3879749d1e54d09bce41c
Andrew Hite <ahite@getsatisfaction.com>
2013-09-11 21:22:06 -0500

    Exclude spec/dummy from SimpleCov for Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'coveralls'
 Coveralls.wear!
 
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter 'spec/dummy'
+end
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment.rb", __FILE__)
 require 'rspec/rails'
