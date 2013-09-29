2e16d8dd590c67b71bd02f180d18ef7715f91e19
Andrew Hite <ahite@getsatisfaction.com>
2013-09-28 22:25:17 -0500

    Tweaks to component example group

--- lib/rafters/rspec/component_example_group.rb
+++ lib/rafters/rspec/component_example_group.rb
@@ -2,16 +2,28 @@ require 'rspec/rails'
 
 module Rafters::ComponentExampleGroup
   extend ActiveSupport::Concern
-  include RSpec::Rails::ViewExampleGroup
+  include RSpec::Rails::RailsExampleGroup
 
   included do
     metadata[:type] = :component
+
+    controller do
+      # Empty controller
+    end
 
     let(:identifier) { described_class.to_s.underscore }
     let(:options) { Hash.new }
 
     subject do
-      described_class.new(identifier, options)
+      described_class.new(identifier, options).tap do |s|
+        s.controller = controller
+      end
+    end
+
+    let(:controller) do
+      example.metadata[:controller].new.tap do |s|
+        s.stub(:params).and_return({})
+      end
     end
 
     let(:renderer) do
@@ -19,11 +31,19 @@ module Rafters::ComponentExampleGroup
     end
 
     let(:page) do
-      Capybara.string renderer.render(subject)
+      Capybara.string(renderer.render(subject))
     end
+  end
 
-    before do
-      subject.controller = controller
+  module ClassMethods
+    def controller(base_class = nil, &body)
+      base_class ||= ApplicationController
+
+      metadata[:controller] = Class.new(base_class) do
+        def self.name; "AnonymousController"; end
+      end
+
+      metadata[:controller].class_eval(&body)
     end
   end
 end
